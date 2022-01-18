21a107d225e35756ebec73614c073252955164d7
Jacob White <jacobewhitedev@gmail.com>
2022-01-18 09:25:20 -0600

    starting transaction card

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,35 @@
 import "package:flutter/material.dart";
+import 'package:intl/intl.dart';
 
-void main() => runApp(MyApp());
+import './transaction.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Flutter App', home: MyHomePage());
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter App',
+        home: MyHomePage());
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 78.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Groceriees', amount: 100.00, date: DateTime.now())
+  ];
+  // String? titleInput;
+  // String? amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -21,17 +38,85 @@ class MyHomePage extends StatelessWidget {
           title: const Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: const [
-            SizedBox(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
               width: double.infinity,
               child: Card(
+                color: Colors.blue,
                 child: Text('Hi there.'),
               ),
             ),
+            // ADD TRANSACTION SECTION
             Card(
-              child: Text('testtest'),
-            )
+              elevation: 5,
+              child: Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      decoration: const InputDecoration(labelText: 'Title'),
+                      controller: titleController,
+                      // onChanged: (val) {
+                      //   titleInput = val;
+                      // },
+                    ),
+                    TextField(
+                      decoration: const InputDecoration(labelText: 'Amount'),
+                      controller: amountController,
+                      // onChanged: (val) {
+                      //   amountInput = val;
+                      // },
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(primary: Colors.purple),
+                      onPressed: () => print(titleController.text),
+                      child: const Text('Add Transaction'),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            // TRANSACTION MAP SECTION
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 50),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2)),
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          '\$${tx.amount}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            tx.title,
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            DateFormat().add_yMMMd().format(tx.date),
+                            style: const TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
+            ),
           ],
         ));
   }
